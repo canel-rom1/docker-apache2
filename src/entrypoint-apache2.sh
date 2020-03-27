28b3e89db48b1528b0c68e6df4439b9cd50167fa
romain <rom1@canel.ch>
2020-03-27 09:32:25 +0100

    Créer une variable d'environnement docker pour le dossier html.

--- src/entrypoint-apache2.sh
+++ src/entrypoint-apache2.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+APACHE_HTML_DIR_TMP=/tmp/www
+
+if [ -d "${APACHE_HTML_DIR_TMP}" ]
+then
+        [ -d "${APACHE_HTML_DIR}" ] && rm -fr "${APACHE_HTML_DIR}"
+        mv "${APACHE_HTML_DIR_TMP}" "${APACHE_HTML_DIR}"
+fi
+
 if [ -n "${APACHE_HTACCESS}" ] && [ "${APACHE_HTACCESS}" -ne 0 ]
 then
         echo "HTACCESS" > /dev/stdout
